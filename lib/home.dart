79ce30e026ea38ee09351c4cf819e7b64b299d05
Olympus9239 <ad9187@srmist.edu.in>
2021-03-21 02:15:09 +0530

    home added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vernacular_health/What_is_Cancer.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,11 +12,97 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Home Page'),
+      appBar: AppBar(
+        title: Text('புற்றுநோய்',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
+        backgroundColor: Colors.indigoAccent,
+        centerTitle: true,
       ),
+      body:Column(
+
+        children:<Widget> [
+          SizedBox(
+            height: 10.0,
+          ),
+          InkWell(
+            onTap: (){},
+            child: Container( 
+              padding: EdgeInsets.all(7),
+              height: 150.0,
+              width: 400.0,
+              decoration: BoxDecoration(
+
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.indigo[700],
+                      Colors.indigo[200]
+                    ],
+                  )
+
+              ),
+              child: Center(child: Text('புற்றுநோய் என்றால் என்ன')),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.all(7),
+              height: 150.0,
+              width: 400.0,
+              decoration: BoxDecoration(
+
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.indigo[700],
+                      Colors.indigo[200]
+                    ],
+                  )
+
+              ),
+              child: Center(child: Text('புற்றுநோய் A-Z')),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.all(7),
+              height: 150.0,
+              width: 400.0,
+              decoration: BoxDecoration(
+
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.indigo[700],
+                      Colors.indigo[200]
+                    ],
+                  )
+
+              ),
+              child: Center(child: Text('செய்தி அறை')),
+            ),
+          ),
+
+
+
+        ],
+      ),
+
     );
   }
 }

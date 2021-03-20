c61fd8354a263f66385351c162ade82b087cb6ae
Ishita <ishita_arora_@outlook.com>
2021-03-21 01:51:54 +0530

    Login page completed

---
+++ lib/content.dart
@@ -0,0 +1,103 @@
+class Content {
+  Map english = {
+    'heading': 'About Cancer',
+    'what': {
+      'title': 'What is Cancer?',
+      'body':
+          'Cancer refers to the abnormal growth of cell tissue.These abnormal cells are termed cancer cells, malignant cells, or tumor cells. These cells can infiltrate normal body tissues.Cancer is not confined to humans only, animals and other living organisms can get cancer too. Frequently, cancer cells can break away from this original mass of cells, travel through the blood and lymph systems, and lodge in other organs where they can again repeat the uncontrolled growth cycle. This process of cancer cells leaving an area and growing in another body area is termed metastatic spread or metastasis'
+    },
+    'stats': {
+      'title': 'Some stats',
+      'body':
+          ' Cancer is a leading cause of death worldwide. It accounted for 8.2 million deaths (around 22% of all deaths not related to communicable diseases; most recent data from WHO).\n●	Lung, stomach, liver, colon, and breast cancer cause the most cancer deaths each year.\n●	Deaths from cancer worldwide are projected to continue rising, with an estimated 13.1 million deaths in 2030 (about a 70% increase).'
+    },
+    'causes': {
+      'title': 'Causes',
+      'body':
+          'There are many causes of cancer, and some are preventable.In addition to smoking, risk factors for cancer include:\n●	heavy alcohol consumption\n●	excess body weight\n●	physical inactivity\n●	poor nutrition'
+    },
+    'genetic': {
+      'title': 'Is cancer genetic?',
+      'body':
+          'Genetic factors can contribute to the development of cancer. A person’s genetic code tells their cells when to divide and expire. Changes in the genes can lead to faulty instructions, and cancer can result. Genes also influence the cells’ production of proteins, and proteins carry many of the instructions for cellular growth and division. Some genes change proteins that would usually repair damaged cells. This can lead to cancer. If a parent has these genes, they may pass on the altered instructions to their offspring. Some genetic changes occur after birth, and factors such as smoking and sun exposure can increase the risk.'
+    },
+    'treatments': {
+      'title': 'Treatments',
+      'body':
+          'Innovative research has fueled the development of new medications and treatment technologies. Doctors usually prescribe treatments based on the type of cancer, its stage at diagnosis, and the person’s overall health.\n●	Stem cell transplant can be especially beneficial for people with blood-related cancers, such as leukemia or lymphoma.\n●	Chemotherapy aims to kill cancerous cells with medications that target rapidly dividing cells. The drugs can also help shrink tumors, but the side effects can be severe\n●	Radiation therapy uses high-dose radiation to kill cancerous cells.\n●	Immunotherapy uses medications and other treatments to boost the immune system and encourage it to fight cancerous cells. \n●	Surgery is often a part of a treatment plan when a person has a cancerous tumor. Also, a surgeon may remove lymph nodes to reduce or prevent the disease’s spread.'
+    },
+    'outlook': {
+      'title': 'Outlook',
+      'body':
+          'Improvements in cancer detection, increased awareness of the risks of smoking, and a drop in tobacco use have all contributed to a year-on-year decrease in the number of cancer diagnoses and deaths. According to the American Cancer Society, the overall cancer death rate declined by 26 percent between 1991 and 2015. When a person has cancer, the outlook will depend on whether the disease has spread and on its type, severity, and location'
+    },
+  };
+
+  Map hindi = {
+    'heading': 'कैंसर के बारे में',
+    'what': {
+      'title': 'कैंसर क्या है?',
+      'body':
+          'कैंसर कोशिका ऊतक की असामान्य वृद्धि को संदर्भित करता है। इन असामान्य कोशिकाओं को कैंसर कोशिका, घातक कोशिका या ट्यूमर कोशिका कहा जाता है। ये कोशिकाएँ सामान्य शरीर के ऊतकों में घुसपैठ कर सकती हैं। नर्तक केवल मनुष्यों तक ही सीमित नहीं है, पशुओं और अन्य जीवों को भी कैंसर हो सकता है। अक्सर, कैंसर कोशिकाएं कोशिकाओं के इस मूल द्रव्यमान से दूर हो सकती हैं, रक्त और लसीका प्रणालियों के माध्यम से यात्रा कर सकती हैं, और अन्य अंगों में घूम सकती हैं जहां वे फिर से अनियंत्रित विकास चक्र दोहरा सकते हैं। कैंसर कोशिकाओं के एक क्षेत्र को छोड़ने और एक अन्य शरीर क्षेत्र में बढ़ने की इस प्रक्रिया को मेटास्टैटिक प्रसार या मेटास्टेसिस कहा जाता है|'
+    },
+    'stats': {
+      'title': 'कुछ आँकड़े',
+      'body':
+          'कैंसर दुनिया भर में मौत का एक प्रमुख कारण है। यह 8.2 मिलियन मौतों (सभी 22% लगभग संक्रामक रोगों से संबंधित नहीं है; डब्ल्यूएचओ के सबसे हालिया आंकड़ों) के लिए जिम्मेदार है।\n•	फेफड़े, पेट, यकृत, बृहदान्त्र और स्तन कैंसर के कारण प्रत्येक वर्ष सबसे अधिक कैंसर होते हैं।\n•	2030 में अनुमानित 13.1 मिलियन मौतों (लगभग 70% वृद्धि) के साथ दुनिया भर में कैंसर से होने वाली मौतों को जारी रखने का अनुमान है।'
+    },
+    'causes': {
+      'title': 'कारण',
+      'body':
+          'कैंसर के कई कारण हैं, और कुछ रोके जा सकते हैं। धूम्रपान के अलावा, कैंसर के जोखिम कारकों में शामिल हैं:\n●	शराब का भारी सेवन\n●	अतिरिक्त शरीर का वजन\n●	भौतिक निष्क्रियता\n●	खराब पोषण'
+    },
+    'genetic': {
+      'title': 'क्या कैंसर आनुवांशिक है?',
+      'body':
+          'आनुवंशिक कारक कैंसर के विकास में योगदान कर सकते हैं। एक व्यक्ति का आनुवांशिक कोड उनकी कोशिकाओं को बताता है कि कब विभाजित और समाप्त होना है। जीन में परिवर्तन से दोषपूर्ण निर्देश हो सकते हैं और कैंसर हो सकता है। जीन भी कोशिकाओं के प्रोटीन के उत्पादन को प्रभावित करते हैं, और प्रोटीन सेलुलर विकास और विभाजन के लिए कई निर्देश देते हैं। कुछ जीन प्रोटीन को बदलते हैं जो आमतौर पर क्षतिग्रस्त कोशिकाओं की मरम्मत करते हैं। इससे कैंसर हो सकता है। यदि किसी माता-पिता के पास ये जीन हैं, तो वे अपने संतानों को परिवर्तित निर्देशों पर पास कर सकते हैं। जन्म के बाद कुछ आनुवंशिक परिवर्तन होते हैं, और धूम्रपान और सूरज के जोखिम जैसे कारक जोखिम को बढ़ा सकते हैं।'
+    },
+    'treatments': {
+      'title': 'उपचार',
+      'body':
+          'नवीन अनुसंधान ने नई दवाओं और उपचार प्रौद्योगिकियों के विकास को बढ़ावा दिया है। डॉक्टर आमतौर पर कैंसर के प्रकार, इसके निदान के चरण और व्यक्ति के समग्र स्वास्थ्य के आधार पर उपचार लिखते हैं|\n-स्टेम सेल ट्रांसप्लांट रक्त से संबंधित कैंसर वाले लोगों के लिए विशेष रूप से फायदेमंद हो सकता है, जैसे कि ल्यूकेमिया या लिम्फोमा।\n-कीमोथेरेपी का उद्देश्य कैंसर कोशिकाओं को दवाओं से मारना है जो तेजी से विभाजित कोशिकाओं को लक्षित करते हैं। ड्रग्स ट्यूमर को कम करने में भी मदद कर सकते हैं, लेकिन दुष्प्रभाव गंभीर हो सकते हैं |\n-विकिरण चिकित्सा कैंसर कोशिकाओं को मारने के लिए उच्च खुराक वाले विकिरण का उपयोग करती है।\n-इम्यूनोथेरेपी प्रतिरक्षा प्रणाली को बढ़ावा देने और कैंसर कोशिकाओं से लड़ने के लिए प्रोत्साहित करने के लिए दवाओं और अन्य उपचारों का उपयोग करती है।\n-सर्जरी अक्सर एक उपचार योजना का एक हिस्सा होता है जब किसी व्यक्ति को कैंसर का ट्यूमर होता है। इसके अलावा, एक सर्जन रोग के प्रसार को कम करने या रोकने के लिए लिम्फ नोड्स को हटा सकता है।'
+    },
+    'outlook': {
+      'title': 'आउटलुक',
+      'body':
+          'कैंसर का पता लगाने में सुधार, धूम्रपान के जोखिमों के बारे में जागरूकता में वृद्धि, और तंबाकू के उपयोग में गिरावट ने कैंसर के निदान और मौतों की संख्या में साल-दर-साल की कमी में योगदान दिया है। अमेरिकन कैंसर सोसाइटी के अनुसार, 1991 और 2015 के बीच कैंसर की कुल मृत्यु दर में 26 प्रतिशत की गिरावट आई है। जब किसी व्यक्ति को कैंसर होता है, तो यह दृष्टिकोण इस बात पर निर्भर करेगा कि क्या बीमारी फैल गई है और उसके प्रकार, गंभीरता और स्थान|'
+    },
+  };
+
+  Map tamil = {
+    'heading': 'புற்றுநோய் பற்றி',
+    'what': {
+      'title': 'புற்றுநோய் என்றால் என்ன?',
+      'body':
+          'புற்றுநோய் என்பது உயிரணு திசுக்களின் அசாதாரண வளர்ச்சியைக் குறிக்கிறது. இந்த அசாதாரண செல்கள் புற்றுநோய் செல்கள், வீரியம் மிக்க செல்கள் அல்லது கட்டி செல்கள் என்று அழைக்கப்படுகின்றன. இந்த செல்கள் சாதாரண உடல் திசுக்களில் ஊடுருவக்கூடும். புற்றுநோய் மனிதர்களுக்கு மட்டுமல்ல, விலங்குகள் மற்றும் பிற உயிரினங்களுக்கும் புற்றுநோய் வரலாம். அடிக்கடி, புற்றுநோய் செல்கள் இந்த அசல் உயிரணுக்களிலிருந்து விலகி, இரத்தம் மற்றும் நிணநீர் அமைப்புகள் வழியாக பயணிக்கலாம், மற்ற உறுப்புகளில் தங்கலாம், அங்கு அவை கட்டுப்பாடற்ற வளர்ச்சி சுழற்சியை மீண்டும் செய்ய முடியும். புற்றுநோய் செல்கள் ஒரு பகுதியை விட்டு வெளியேறி மற்றொரு உடல் பகுதியில் வளரும் இந்த செயல்முறை மெட்டாஸ்டேடிக் பரவல் அல்லது மெட்டாஸ்டாஸிஸ் என்று அழைக்கப்படுகிறது'
+    },
+    'stats': {
+      'title': 'சில புள்ளிவிவரங்கள்',
+      'body':
+          ' உலகளவில் மரணத்திற்கு புற்றுநோய் ஒரு முக்கிய காரணம். இது 8.2 மில்லியன் இறப்புகளுக்குக் காரணமாக இருந்தது (மொத்த இறப்புகளில் 22% தொற்று நோய்களுடன் தொடர்புடையது அல்ல; WHO இன் சமீபத்திய தகவல்கள்).\n-நுரையீரல், வயிறு, கல்லீரல், பெருங்குடல் மற்றும் மார்பக புற்றுநோய் ஆகியவை ஒவ்வொரு ஆண்டும் அதிக புற்றுநோய் இறப்பை ஏற்படுத்துகின்றன.\n-உலகளவில் புற்றுநோயால் ஏற்படும் இறப்புகள் தொடர்ந்து உயரும் என்று கணிக்கப்பட்டுள்ளது, 2030 ஆம் ஆண்டில் 13.1 மில்லியன் இறப்புகள் மதிப்பிடப்பட்டுள்ளன (சுமார் 70% அதிகரிப்பு).'
+    },
+    'causes': {
+      'title': 'காரணங்கள்',
+      'body':
+          'புற்றுநோய்க்கு பல காரணங்கள் உள்ளன, மேலும் சில தடுக்கக்கூடியவை. புகைபிடிப்பதைத் தவிர, புற்றுநோய்க்கான ஆபத்து காரணிகள் பின்வருமாறு:\n-அதிக மது அருந்துதல்\n-அதிக உடல் எடை\n-உடல் செயலற்ற தன்மை\n-மோசமான ஊட்டச்சத்து'
+    },
+    'genetic': {
+      'title': 'புற்றுநோய் மரபணு?',
+      'body':
+          'புற்றுநோயின் வளர்ச்சிக்கு மரபணு காரணிகள் பங்களிக்கக்கூடும். ஒரு நபரின் மரபணுக் குறியீடு அவற்றின் கலங்களை எப்போது பிரித்து காலாவதியாகும் என்று சொல்கிறது. மரபணுக்களில் ஏற்படும் மாற்றங்கள் தவறான வழிமுறைகளுக்கு வழிவகுக்கும், மேலும் புற்றுநோய் ஏற்படலாம். உயிரணுக்களின் புரதங்களின் உற்பத்தியையும் மரபணுக்கள் பாதிக்கின்றன, மேலும் செல்லுலார் வளர்ச்சி மற்றும் பிரிவுக்கான பல வழிமுறைகளை புரதங்கள் கொண்டு செல்கின்றன. சில மரபணுக்கள் பொதுவாக சேதமடைந்த செல்களை சரிசெய்யும் புரதங்களை மாற்றுகின்றன. இது புற்றுநோய்க்கு வழிவகுக்கும். ஒரு பெற்றோருக்கு இந்த மரபணுக்கள் இருந்தால், அவர்கள் மாற்றப்பட்ட வழிமுறைகளை தங்கள் சந்ததியினருக்கு அனுப்பலாம். பிறப்புக்குப் பிறகு சில மரபணு மாற்றங்கள் நிகழ்கின்றன, மேலும் புகைபிடித்தல் மற்றும் சூரிய வெளிப்பாடு போன்ற காரணிகள் ஆபத்தை அதிகரிக்கும்.'
+    },
+    'treatments': {
+      'title': 'சிகிச்சைகள்',
+      'body':
+          'புதுமையான ஆராய்ச்சி புதிய மருந்துகள் மற்றும் சிகிச்சை தொழில்நுட்பங்களின் வளர்ச்சியைத் தூண்டியுள்ளது. புற்றுநோயின் வகை, நோயறிதலில் அதன் நிலை மற்றும் நபரின் ஒட்டுமொத்த ஆரோக்கியம் ஆகியவற்றின் அடிப்படையில் மருத்துவர்கள் வழக்கமாக சிகிச்சைகள்\n-பரிந்துரைக்கின்றனர்.\n-இரத்த சம்பந்தப்பட்ட புற்றுநோய்களான லுகேமியா அல்லது லிம்போமா போன்றவர்களுக்கு ஸ்டெம் செல் மாற்று குறிப்பாக நன்மை பயக்கும்.\n-கீமோதெரபி புற்றுநோய் செல்களை விரைவாகப் பிரிக்கும் உயிரணுக்களை குறிவைக்கும் மருந்துகளுடன் கொல்லுவதை நோக்கமாகக் கொண்டுள்ளது. கட்டிகள் சுருங்குவதற்கு மருந்துகள் உதவக்கூடும், ஆனால் பக்க விளைவுகள் கடுமையாக இருக்கும்\n-கதிர்வீச்சு சிகிச்சை புற்றுநோய் செல்களைக் கொல்ல அதிக அளவு கதிர்வீச்சைப் பயன்படுத்துகிறது.\n-நோயெதிர்ப்பு சக்தியை அதிகரிப்பதற்கும் புற்றுநோய் செல்களை எதிர்த்துப் போராட ஊக்குவிப்பதற்கும் நோயெதிர்ப்பு சிகிச்சை மருந்துகள் மற்றும் பிற சிகிச்சைகளைப் பயன்படுத்துகிறது.\n-ஒரு நபருக்கு புற்றுநோய் கட்டி இருக்கும்போது அறுவை சிகிச்சை என்பது பெரும்பாலும் சிகிச்சை திட்டத்தின் ஒரு பகுதியாகும். மேலும், நோய் பரவுவதைக் குறைக்க அல்லது தடுக்க ஒரு அறுவை சிகிச்சை நிபுணர் நிணநீர் முனைகளை அகற்றலாம்.'
+    },
+    'outlook': {
+      'title': 'அவுட்லுக்',
+      'body':
+          'புற்றுநோயைக் கண்டறிவதில் முன்னேற்றம், புகைபிடிப்பதால் ஏற்படும் அபாயங்கள் குறித்த விழிப்புணர்வு மற்றும் புகையிலை பயன்பாட்டின் வீழ்ச்சி ஆகியவை புற்றுநோயைக் கண்டறிதல் மற்றும் இறப்புகளின் எண்ணிக்கையில் ஆண்டுக்கு ஆண்டு குறைவதற்கு பங்களித்தன. அமெரிக்கன் கேன்சர் சொசைட்டியின் கூற்றுப்படி, 1991 மற்றும் 2015 க்கு இடையில் ஒட்டுமொத்த புற்றுநோய் இறப்பு விகிதம் 26 சதவீதம் குறைந்துள்ளது. ஒரு நபருக்கு புற்றுநோய் இருக்கும்போது, நோய் பரவியுள்ளதா மற்றும் அதன் வகை, தீவிரம் மற்றும் இருப்பிடம் ஆகியவற்றைப் பொறுத்தது.'
+    },
+  };
+}

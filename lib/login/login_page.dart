886ef13e15f16910088fece5cc62313b771bb3fc
naman-jn <namanjn1534@gmail.com>
2021-03-20 10:21:09 +0530

    initial commit

---
+++ lib/login/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('Login Page'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vernacular_health/login/login_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vernacular Health',
+      debugShowCheckedModeBanner: false,
+      home: CheckUserStatus(),
+    );
+  }
+}
+
+class CheckUserStatus extends StatefulWidget {
+  CheckUserStatus({Key key}) : super(key: key);
+
+  @override
+  _CheckUserStatusState createState() => _CheckUserStatusState();
+}
+
+class _CheckUserStatusState extends State<CheckUserStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: checkSignIn(),
+        builder: (context, snapshot) {
+          var page;
+          if (snapshot.hasData) {
+            switch (snapshot.data[1]) {
+              case "login":
+                {
+                  page = LoginPage();
+                  break;
+                }
+              case "home":
+                {
+                  // page = HomePage(uid: snapshot.data[0]);
+                  break;
+                }
+            }
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: page,
+            );
+          } else {
+            return Container(
+              color: Colors.white,
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [Center(child: CircularProgressIndicator())],
+              ),
+            );
+          }
+        });
+  }
+
+  Future<List<String>> checkSignIn() {
+    return Future.value(['notSignedIn', "login"]);
+  }
+}
